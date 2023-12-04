a34b92d6aefb72994782f29ca09fbb53f51418c9
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-12-04 15:21:31 -0800

    se arregaron errores de main.h y printf.c

--- main.h
+++ main.h
@@ -16,12 +16,22 @@ struct convert
 	char *symbol;
 	int (*function)(va_list);
 };
-typedef struct convert convert_t
+typedef struct convert convert_t;
 
 /**
  * prototypes:
  */
 
 int _printf(const char *format, ...);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int unsigned_integer(va_list);
+int print_unsgned_number(unsigned int);
+int print_reversed(va_list);
+int recording_error(const char *format, convert_t f_list[], va_list arg_list);
 
 #endif

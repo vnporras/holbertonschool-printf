db2d6fa39f97da6a9b12b7f03fcdb4a2923c0653
vnporras <vnporras1028@gmail.com>
2023-12-04 19:15:03 -0800

    crerate the _printf and main.h

---
+++ _printf.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+   
+/**
+ * _printf - function that produces output according to a format.
+ * @format: Is a pointer strings.
+ *
+ * Return - a count characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int formatCopy = 0;
+	int countCharacters = 0; 
+	va_list args; 
+	va_start(args, format);
+	while (format[formatCopy] != '\0')
+	{
+		if (format[formatCopy] == '%') 
+		{
+			formatCopy++;
+			if (format[formatCopy] == 'c')
+			{
+				char c = va_arg(args, int); 
+				write(1, &c, 1);
+				countCharacters++;
+			}
+			else if (format[formatCopy] == 's')
+			{
+				char *s = va_arg(args, char *); 
+				int sCopy = 0; 
+				while (s[sCopy] != '\0')
+				{
+					write(1, &s[sCopy], 1);
+					sCopy++;
+					countCharacters++;
+				}
+			}
+			else if (format[formatCopy] == '%')
+			{
+				write(1, &format[formatCopy], 1);
+				countCharacters++;
+			}
+		}
+		else
+		{
+			write(1, &format[formatCopy], 1); 
+			countCharacters++;
+		}
+		formatCopy++;
+	}
+	va_end(args);
+	return (countCharacters);
+}
---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct convert - associate symbols with functions
+ * @symbol: pointer to a character that contains a symbol or a string
+ * @function: pointer to a function that takes an argument of type 
+ * va_list and returns an integer
+ */
+
+struct convert
+{
+	char *symbol;
+	int (*function)(va_list);
+};
+typedef struct convert convert_t;
+
+/**
+ * prototypes:
+ */
+
+int _printf(const char *format, ...);
+#endif

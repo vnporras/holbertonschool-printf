7ecf43d4212155608a9aceafdff3ca82720a42bb
vnporras <vnporras1028@gmail.com>
2023-12-05 08:32:43 -0800

    fix conflics in file for betty style

--- _printf.c
+++ _printf.c
@@ -2,34 +2,37 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-   
 /**
  * _printf - function that produces output according to a format.
  * @format: Is a pointer strings.
  *
- * Return - a count characters
+ * Return: a count characters
 */
 int _printf(const char *format, ...)
 {
 	int formatCopy = 0;
-	int countCharacters = 0; 
-	va_list args; 
+	int countCharacters = 0;
+	va_list args;
+
 	va_start(args, format);
+
 	while (format[formatCopy] != '\0')
 	{
-		if (format[formatCopy] == '%') 
+		if (format[formatCopy] == '%')
 		{
 			formatCopy++;
 			if (format[formatCopy] == 'c')
 			{
-				char c = va_arg(args, int); 
+				char c = va_arg(args, int);
+
 				write(1, &c, 1);
 				countCharacters++;
 			}
 			else if (format[formatCopy] == 's')
 			{
-				char *s = va_arg(args, char *); 
-				int sCopy = 0; 
+				char *s = va_arg(args, char *);
+				int sCopy = 0;
+
 				while (s[sCopy] != '\0')
 				{
 					write(1, &s[sCopy], 1);
@@ -45,7 +48,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			write(1, &format[formatCopy], 1); 
+			write(1, &format[formatCopy], 1);
 			countCharacters++;
 		}
 		formatCopy++;

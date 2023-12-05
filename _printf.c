668ae29b1807ca85dd1ef9e07d127ea4e8ea440a
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-12-05 13:01:20 -0800

    Guardando cambios en _printf.c antes de cambiar de rama

--- _printf.c
+++ _printf.c
@@ -2,50 +2,36 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-   
 /**
  * _printf - function that produces output according to a format.
  * @format: Is a pointer strings.
  *
- * Return - a count characters
+ * Return: a count characters.
 */
 int _printf(const char *format, ...)
 {
 	int formatCopy = 0;
 	int countCharacters = 0;
-	int sCopy = 0;
 	va_list args;
+	int sCopy = 0;
 
 	va_start(args, format);
+
 	while (format[formatCopy] != '\0')
 	{
-		if (format[formatCopy] == '%') 
+		if (format[formatCopy] == '%')
 		{
 			formatCopy++;
 			if (format[formatCopy] == 'c')
 			{
-				char c = va_arg(args, int); 
+				char c = va_arg(args, int);
+
 				write(1, &c, 1);
 				countCharacters++;
 			}
 			else if (format[formatCopy] == 's')
 			{
 				char *s = va_arg(args, char *);
-
-				if (s == NULL)
-				{
-					write(1, "(null)", 6);
-					countCharacters += 6;
-				}
-				else
-				{
-					while (s[sCopy] != '\0')
-					{
-						write(1, &s[sCopy], 1);
-						sCopy++;
-						countCharacters++;
-					}
-				}
 
 				while (s[sCopy] != '\0')
 				{
@@ -59,10 +45,22 @@ int _printf(const char *format, ...)
 				write(1, &format[formatCopy], 1);
 				countCharacters++;
 			}
+			else if (format[formatCopy] == 'd')
+			{
+				int d = va_arg(args, int);
+				write(1, &d, 1);
+				countCharacters++;
+			}
+			else if (format[formatCopy] == 'i')
+			{
+				int i = va_arg(args, int);
+				write(1, &i, 1);
+				countCharacters++;
+			}
 		}
 		else
 		{
-			write(1, &format[formatCopy], 1); 
+			write(1, &format[formatCopy], 1);
 			countCharacters++;
 		}
 		formatCopy++;

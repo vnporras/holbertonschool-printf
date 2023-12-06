2daa4bb0e1006fe0f9b0f4c524bcf3786a790c94
Vanessa Porras <79481309+vnporras@users.noreply.github.com>
2023-12-06 00:09:43 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,11 +9,8 @@ int _printf(const char *format, ...)
 {
 	int i = 0;
 	int countCharacters = 0;
-<<<<<<< HEAD
-=======
 	int sCopy = 0;
-  
->>>>>>> 225985c57303bc360ab7a776f343429559fcfee8
+
 	va_list args;
 
 	va_start(args, format);
@@ -21,14 +18,13 @@ int _printf(const char *format, ...)
 	for (; format[i] != 0; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '0')
+		{
 			return (-1);
-
+		}
 		if (format[i] == 'c')
 		{
 			char c = va_arg(args, int);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
+			
 			write(1, &c, 1);
 			countCharacter++;
 		}
@@ -36,7 +32,7 @@ int _printf(const char *format, ...)
 		{	
 			int i = 0;
 			char *s = va_arg(args, char *);
-=======
+
 			if (format[formatCopy] == '\0')
 			{
 				write(1, "%", 1);
@@ -46,10 +42,9 @@ int _printf(const char *format, ...)
 			if (format[formatCopy] == 'c')
 			{
 				char c = va_arg(args, int);
->>>>>>> d2c9f9b02f80511ebdd3c6417901e217713c7567
-
+			}
 			if (s == NULL)
-=======
+			{
 				write(1, &c, 1);
 				countCharacters++;
 			}
@@ -58,7 +53,6 @@ int _printf(const char *format, ...)
 				char *s = va_arg(args, char *);
         
 				if (s == NULL)
->>>>>>> 225985c57303bc360ab7a776f343429559fcfee8
 				{
 					s = "(null)";
 				}
@@ -70,14 +64,14 @@ int _printf(const char *format, ...)
 						i++;
 					}
 				}
+			}
+			else
+			{
+				write(1, &format[i], 1);
+				countCharacters++;
+			}
+			i++;
 		}
-		else
-		{
-			write(1, &format[i], 1);
-			countCharacters++;
-		}
-		i++;
+		va_end(args);
+		return (countCharacters);
 	}
-	va_end(args);
-	return (countCharacters);
-}

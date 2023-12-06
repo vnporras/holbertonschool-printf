4790120cbea1b06d3034c298545dce7cf38d569a
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-12-05 17:56:53 -0800

    se arreglaron errores 1, 3, y 5

--- _printf.c
+++ _printf.c
@@ -12,8 +12,8 @@ int _printf(const char *format, ...)
 {
 	int formatCopy = 0;
 	int countCharacters = 0;
-	va_list args;
 	int sCopy = 0;
+	va_list args;
 
 	va_start(args, format);
 
@@ -22,6 +22,13 @@ int _printf(const char *format, ...)
 		if (format[formatCopy] == '%')
 		{
 			formatCopy++;
+
+			if (format[formatCopy] == '\0') //en caso de que solo se proporcione "%" o un solo caracter sin especificador de formato
+			{
+				write(1, "%", 1);
+				countCharacters++;
+				break;
+			}
 			if (format[formatCopy] == 'c')
 			{
 				char c = va_arg(args, int);
@@ -33,6 +40,21 @@ int _printf(const char *format, ...)
 			{
 				char *s = va_arg(args, char *);
 
+				//para el manejo de caracter nulo 
+				if (s == NULL)
+				{
+					write(1, "(null)", 6);
+					countCharacters += 6;
+				}
+				else
+				{
+					while (s[sCopy] != '\0')
+					{
+						write(1, &s[sCopy], 1);
+						sCopy++;
+						countCharacters++;
+					}
+				}
 				while (s[sCopy] != '\0')
 				{
 					write(1, &s[sCopy], 1);
